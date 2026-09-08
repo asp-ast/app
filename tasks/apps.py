1ca69825ce6941b02e4c7d7f27d006577c7d45e7
Alexandr Sotnikov <sashasotnikov882@gmail.com>
2026-09-09 00:21:42 +0300

    chore(tasks): customed tasks app config

--- tasks/apps.py
+++ tasks/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
+    """Настраивает Django-приложение для управления задачами."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+    verbose_name = 'Управление задачами'
