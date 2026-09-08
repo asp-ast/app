0a5488bb492c75438960f567539e619801eb75f8
Alexandr Sotnikov <sashasotnikov882@gmail.com>
2026-09-09 00:32:58 +0300

    feat(tasks): added tasks models to django-admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, TaskPriority, TaskStatus
+
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    """Настраивает административный интерфейс для статусов задач."""
+
+    list_display = (
+        "name",
+        "code",
+    )
+
+
+@admin.register(TaskPriority)
+class TaskPriorityAdmin(admin.ModelAdmin):
+    """Настраивает административный интерфейс для приоритетов задач."""
+
+    list_display = (
+        "name",
+        "level",
+    )
+    ordering = (
+        "level",
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Настраивает административный интерфейс для задач."""
+
+    list_display = (
+        "title",
+        "status",
+        "priority",
+        "estimated_hours",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "priority",
+    )
+    search_fields = (
+        "title",
+        "description",
+    )
+    list_select_related = (
+        "status",
+        "priority",
+    )
